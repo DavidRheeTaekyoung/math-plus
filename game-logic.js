c77d825af0f1dc3cae2cfec407e3cecd702efea2
DavidRheeTaekyoung <prodigen@gmail.com>
2026-01-12 19:52:15 +0900

    Add files via upload

--- game-logic.js
+++ game-logic.js
@@ -73,13 +73,63 @@ class GameLogic {
             this.currentAns = n1 + n2;
             this.currentOperator = '+';
             this.levelMultiplier = 4.0;
-        } else {
+        } else if (difficulty === 8) {
             // Level 8: 두자리에서 두자리 빼기
             n1 = this.r(20, 99); // 최소 20으로 설정하여 n2가 10 이상이 되도록
             n2 = this.r(10, n1 - 1);
             this.currentAns = n1 - n2;
             this.currentOperator = '-';
             this.levelMultiplier = 6.0; // 더하기 4보다 2점 높게
+        } else if (difficulty === 9) {
+            // Level 9: 구구단 (곱해서 20 이하)
+            do {
+                n1 = this.r(2, 9);
+                n2 = this.r(2, 9);
+            } while (n1 * n2 > 20);
+            this.currentAns = n1 * n2;
+            this.currentOperator = '×';
+            this.levelMultiplier = 7.0;
+        } else if (difficulty === 10) {
+            // Level 10: 구구단 (곱해서 20 초과)
+            do {
+                n1 = this.r(2, 9);
+                n2 = this.r(2, 9);
+            } while (n1 * n2 <= 20);
+            this.currentAns = n1 * n2;
+            this.currentOperator = '×';
+            this.levelMultiplier = 8.0;
+        } else if (difficulty === 11) {
+            // Level 11: 2의 거듭제곱 (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024)
+            const powers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // 2^0 ~ 2^10
+            const power = powers[this.r(0, powers.length - 1)];
+            n1 = 2;
+            n2 = power;
+            this.currentAns = Math.pow(2, power);
+            this.currentOperator = '^';
+            this.levelMultiplier = 9.0;
+        } else if (difficulty === 12) {
+            // Level 12: 3의 거듭제곱 (1, 3, 9, 27, 81, 243)
+            const powers = [0, 1, 2, 3, 4, 5]; // 3^0 ~ 3^5
+            const power = powers[this.r(0, powers.length - 1)];
+            n1 = 3;
+            n2 = power;
+            this.currentAns = Math.pow(3, power);
+            this.currentOperator = '^';
+            this.levelMultiplier = 9.0;
+        } else if (difficulty === 13) {
+            // Level 13: 두자리수 × 한자리수 (10~19 × 1~9)
+            n1 = this.r(10, 19);
+            n2 = this.r(1, 9);
+            this.currentAns = n1 * n2;
+            this.currentOperator = '×';
+            this.levelMultiplier = 10.0;
+        } else {
+            // Level 14: 제곱수 외우기 (1² ~ 19²)
+            n1 = this.r(1, 19);
+            n2 = 2; // 제곱 표시용
+            this.currentAns = n1 * n1;
+            this.currentOperator = '²';
+            this.levelMultiplier = 11.0;
         }
         
         return {
